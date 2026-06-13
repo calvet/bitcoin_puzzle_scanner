ede03b4a5bc1f49af726f99753616d5768755156
Wallace Calvet <31490548+calvet@users.noreply.github.com>
2026-06-13 17:12:07 +0100

    Deleted non used files

--- test_hash3.py
+++
@@ -1,4 +0,0 @@
-import base58
-addr = "1BgGZ9tcN4rm9KBzDn7KprQz87SZ26SAMH"
-decoded = base58.b58decode_check(addr)
-print("Base58 decoded:", decoded.hex())
--- test_modmul.cpp
+++
@@ -1,7 +0,0 @@
-#include "secp256k1/Int.h"
-#include <iostream>
-
-int main() {
-    // Cannot compile easily, I'll just check IntMod.cpp
-    return 0;
-}
